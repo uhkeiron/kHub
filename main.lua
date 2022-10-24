0353f3bfe698c71beb974af67b39625839a3f5be
Ausicius <ausicius@gmail.com>
2022-10-24 13:33:18 +0700

    1.2.8

--- main.lua
+++ main.lua
@@ -1,22 +1,23 @@
 --<< Functions >>--
-local function CreateLoadstring(gameName, placeIds, loadstring)
-    return (function() -- I do this because I am bored :P
-        placeIds = placeIds or {}
+local function RunAndCheckPlaceID(gameName, placeIds, func)
+    placeIds = placeIds or {}
+    func = func or function() end
 
-        print("Running ", gameName)
-        for index, placeId in ipairs(placeIds) do
-            if (placeId == 0) then
-                loadstring(game:HttpGet(loadstring))()
-                break
-            end
-    
-            if (game.PlaceId == placeId) then
-                loadstring(game:HttpGet(loadstring))()
-            end
+    print("Running ", gameName)
+    for index, placeId in ipairs(placeIds) do
+        if (placeId == 0) then
+            func()
+            break
         end
-    end)
+
+        if (game.PlaceId == placeId) then
+            func()
+        end
+    end
 end
 
 
 --<< Main Code >>--
-CreateLoadstring("Universal", {0}, "https://raw.githubusercontent.com/Ausicius/AscHub-Roblox/master/games/!universal.lua")()
+RunAndCheckPlaceID("Universal", {0}, function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ausicius/AscHub-Roblox/master/games/!universal.lua"))()
+end)
